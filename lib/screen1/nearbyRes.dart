aa6abf58ba9cff64af2c669cdb054e7d50444476
Harmanjit Singh <harmanjit140500@gmail.com>
2020-08-18 21:01:41 +0530

    static screen 1 done

--- lib/screen1/nearbyRes.dart
+++ lib/screen1/nearbyRes.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:food_app_ui/data/data.dart';
 import 'package:food_app_ui/models/restaurant.dart';
 
+Widget rating(BuildContext context, int rat) {
+  List<Widget> stars = [];
+  for (int i = 1; i <= rat; i++) {
+    stars.add(Icon(Icons.star));
+  }
+  return Row(
+    children: stars,
+  );
+}
+
 Widget nearbyRes(BuildContext context) {
   List<Widget> restList = [];
   restaurants.forEach((Restaurant restaurant) {
     restList.add(
       Container(
-        height: MediaQuery.of(context).size.height * 0.2,
+        height: MediaQuery.of(context).size.height * 0.15,
         margin: EdgeInsets.fromLTRB(20, 15, 20, 0),
         decoration: BoxDecoration(
           color: Colors.grey[350],
@@ -30,15 +40,23 @@ Widget nearbyRes(BuildContext context) {
             Container(
               margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     restaurant.name,
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Text(
                     restaurant.address,
                     style: TextStyle(fontSize: 15),
                   ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  rating(context, restaurant.rating),
                 ],
               ),
             ),
